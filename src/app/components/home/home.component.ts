30c9fe3f908439bc565587054ecb18e83f12067d
thiago@toribeiro.com <thiago@toribeiro.com>
2020-01-10 16:13:03 -0600

    Removing logic from home and calling children

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
 @Component({
   selector: 'app-home',
@@ -7,15 +7,7 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 })
 
 export class HomeComponent implements OnInit {
-  @ViewChild('formattedTime', {static: false}) formattedTime: any;
-  bttnStrArr = [ButtonTitles.Start, ButtonTitles.Reset, ButtonTitles.Finish];
-  formattedTimeStr: string;
-  tasks = [];
-  shouldAskForTag = false;
-  taskDescFormatted: string;
   sessionInfo: string;
-  worker = new Worker('../../app.worker', { type: 'module' });
-  elapsedTime = '00:00';
 
   constructor(private deviceService: DeviceDetectorService) { }
 
@@ -28,130 +20,4 @@ export class HomeComponent implements OnInit {
     + this.deviceService.browser + " " + parseInt(this.deviceService.browser_version);
   }
 
-  start() {
-    this.postMessageToWorker(ButtonTitles.Start);
-    if (this.bttnStrArr[0] === ButtonTitles.Start || this.bttnStrArr[0] === ButtonTitles.Continue) {
-      this.bttnStrArr[0] = ButtonTitles.Stop;
-    } 
-  }
-
-  reset () {
-    this.postMessageToWorker(ButtonTitles.Reset);
-    this.bttnStrArr[0] = ButtonTitles.Start;
-  }
-
-  stop () {
-    this.postMessageToWorker(ButtonTitles.Stop);
-    if (this.bttnStrArr[0] === ButtonTitles.Stop) {
-      this.bttnStrArr[0] = ButtonTitles.Continue;
-    } 
-  }
-
-  postMessageToWorker(message: string) {
-    if (typeof Worker !== 'undefined') {
-      this.worker.postMessage(message);
-      this.worker.onmessage = ({ data }) => {
-        this.elapsedTime = data;
-      };
-    }
-  }
-
-  handleBtnAction(event: any) {
-    var btnTxt = event.target.innerText;
-    if (btnTxt === ButtonTitles.Start) {
-      this.start();
-    } else if (btnTxt === ButtonTitles.Stop) {
-      this.stop();
-    } else if (btnTxt === ButtonTitles.Continue) {
-      this.start();
-    } else if (btnTxt === ButtonTitles.Reset) {
-      this.reset();
-    } else if (btnTxt === ButtonTitles.Finish) {
-      this.finish();
-    }
-  }
-
-  finish() {
-    this.reset();
-    this.formattedTimeStr = this.formattedTime.nativeElement.innerText;
-    var now = new Date();
-    var h = now.getHours();
-    var m = now.getMinutes();
-    var s = now.getSeconds();
-
-    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-    
-    var day = days[ now.getDay() ];
-    var month = months[ now.getMonth() ];
-
-    var taskTagStr = " - ";
-    if (this.shouldAskForTag) {
-      var taskTag = prompt("Enter a name for this task:");
-      if (taskTag) taskTag = " - " + taskTag;
-      taskTagStr = taskTag + " - ";
-    }
-    this.taskDescFormatted = this.formattedTimeStr + taskTagStr + day + ", " + month + " " + now.getDate() + ", " + now.getFullYear() + " at " + this.addLeadingZero(h) + ":" + this.addLeadingZero(m) + ":" + this.addLeadingZero(s);
-    this.tasks.push(this.taskDescFormatted);
-    this.bttnStrArr[0] = ButtonTitles.Start;
-  }
-
-  handleTagCheck() {
-    this.shouldAskForTag = !this.shouldAskForTag;
-  }
-
-  deleteTask(index: number) {
-    this.tasks.splice(index, 1);
-  }
-
-  addLeadingZero(n: any) {
-    if (n <= 9) {
-      return "0" + n.toString();
-    }
-    return n.toString();
-  }
-
-  export() {
-    var fileName = "thyme-tasks-"+ new Date().toLocaleString()  + ".txt";
-    var taskLines = "";
-    this.tasks.forEach(task => {
-      taskLines += task + "\n";
-    });
-    this.saveTextAsFile(taskLines, fileName);
-  }
-
-  saveTextAsFile (data: BlobPart, filename: string) { 
-    if (!data) {
-      console.error('Console.save: No data')
-      return;
-    }
-
-    if (!filename) filename = 'console.json'
-
-    var blob = new Blob([data], {type: 'text/plain'});
-    var e = document.createEvent('MouseEvents');
-    var a = document.createElement('a');
-
-    // FOR IE:
-    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-      window.navigator.msSaveOrOpenBlob(blob, filename);
-    } else {
-      var e = document.createEvent('MouseEvents');
-      var a = document.createElement('a');
-
-      a.download = filename;
-      a.href = window.URL.createObjectURL(blob);
-      a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
-      e.initEvent('click', true, false);
-      a.dispatchEvent(e);
-    }
-  }
-}
-
-enum ButtonTitles {
-  Start = 'Start',
-  Stop = 'Stop',
-  Reset = 'Reset',
-  Finish = 'Finish',
-  Continue = 'Continue'
 }
